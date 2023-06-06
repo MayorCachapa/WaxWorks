bac0bcbc362505ae33e173fa8ecd3aab79a2fa0f
Parker Budding <parkerbudding@gmail.com>
2023-06-06 13:28:34 +0200

    controllers finished with skeleton framework

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,11 @@
+class FavoritesController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,10 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
