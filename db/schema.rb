4e2dfa956a81adb1fe8a1185cfeaa8997299e45f
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-06-07 09:22:39 +0200

    Recreated Listings Controller using command line

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_064758) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_070734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,9 +31,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_064758) do
     t.float "price"
     t.float "shipping_fee"
     t.text "comments"
-    t.boolean "allow_offers"
+    t.boolean "allow_offers", default: true
     t.string "location"
-    t.boolean "availability"
+    t.boolean "availability", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "release_id", null: false
