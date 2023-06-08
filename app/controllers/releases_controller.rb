082c2fdbaa3f68dd995a6dbc8d06c5e4711ec9b1
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-06-08 16:21:20 +0200

    Reworked Listings and Releases
    Added features:
    New Listing and Release chained togeter.
    Releases do not duplicate
    Tested and validated through Discogs

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,26 +1,53 @@
+require 'httparty'
+
 class ReleasesController < ApplicationController
-    before_action :set_release, only: [:show]
+  before_action :set_release, only: [:show]
 
-    def index
-      @releases = Release.all
-      @listings = Listing.all
+  def index
+    @releases = Release.all
+    @listings = Listing.all
+  end
+
+  def new
+    @release = Release.new
+  end
+
+  def create
+    # raise
+    response = HTTParty.get("https://api.discogs.com/database/search?q=#{params[:release][:title]}&token=#{ENV['DISCOG_TOKEN']}")
+    data = JSON.parse(response.body)
+
+    if data['results'].any?
+      result = data['results'].first['title'].split(' - ')
+      url = data['results'].first['cover_image']
+      artist = result[0].strip
+      title = result[1].strip
+
+      @release = Release.find_by(artist: artist, title: title)
+      if @release
+        redirect_to new_release_listing_path(@release)
+      else
+        @release = Release.new(artist: artist, title: title, url: url)
+        if @release.save
+          redirect_to new_release_listing_path(@release)
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
     end
+  end
 
-    def new
-      @release = Release.new
-      @listing = Listing.new
-    end
+  def show
+  end
 
-    def create
-      
-    end
+  private
 
-    def show
-    end
+  def set_release
+    @release = Release.find(params[:id])
+  end
 
-    private
-
-    def set_release
-      @release = Release.find(params[:id])
-    end
+  def release_params
+      params.require(:release).permit(:title)
+  end
 end
