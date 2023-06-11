694bcb66bb1cc689a525bc88d9fe2c5c92a70bd7
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-06-12 01:48:46 +0200

    Added columns to User and fetches data correctly from Spotify

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,9 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.full_name = auth.info.name
       user.avatar_url = auth.info.image
+      user.spotify_token = auth.credentials.token
+      user.spotify_refresh_token = auth.credentials.refresh_token
+      user.spotify_token_expires_at = Time.at(auth.credentials.expires_at)
     end
   end
   validates :full_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_175351) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_231134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -156,6 +156,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_175351) do
     t.string "avatar_url"
     t.string "provider"
     t.string "full_name"
+    t.string "spotify_token"
+    t.string "spotify_refresh_token"
+    t.string "spotify_token_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
