08329e99d7102bb01d6712a6d4f4b03f3ac5d6ca
Jared Champayne <j4champa@gmail.com>
2023-06-11 17:50:28 +0200

    favorites index updated - linking from release button to index incomplete

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,19 +2,20 @@ class FavoritesController < ApplicationController
     def index
         @favorites = Favorite.all
     end
-    
+
     def create
         @favorite = Favorite.new
+
         @favorites.user = current_user
-        @favorites.release = params[:release_id]
-    
+        @favorites.release = Release.find(params[:release_id])
+
         if @favorite.save
           redirect_to release_path(@favorite.release)
         else
           render :index, status: :unprocessable_entity
         end
     end
-    
+
     def update
     end
 end
